ec93333b278c298211f7418bfba150f876df6d02
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 00:26:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function add(a, b){
-  return a+b
-}
-
-
-
-// var a, b
-
-// beforeEach(function() {
-//   a = Math.floor(Math.random() * 1000)
-//   b = Math.floor(Math.random() * 1000)
-// })
-
 
 
 function add(a, b){
@@ -41,25 +28,6 @@ function makeInt(a){
   return parseInt(a, 10)
 }
 
-unction makeInt(a){
-  return parseInt(a, 10)
+function makeFloatt(a){
+  return parseFloat(a)
 }
-
-//   it('assumes base 10', function() {
-//     expect(makeInt('0x2328')).toEqual(0)
-//   })
-
-//   it('returns NaN as appropriate', function() {
-//     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-//   })
-// })
-
-// describe('preserveDecimal(n)', function() {
-//   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-//     expect(preserveDecimal('2.222')).toBe(2.222)
-//   })
-
-//   it('returns NaN as appropriate', function() {
-//     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-//   })
-// })
